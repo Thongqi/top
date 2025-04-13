2cd38318a6bbc98e3f482ed67ea390b31b305c3b
Thongqi <tsecheew@gmail.com>
2025-04-13 11:35:26 +0800

    done

--- card-slider/card-slider.js
+++ card-slider/card-slider.js
@@ -1,26 +1,86 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const imageList = document.querySelector('.card-slider .image-container');
-    const scrollableLength = imageList.scrollWidth - imageList.clientWidth;
+   document.addEventListener("DOMContentLoaded", function (){
+        const imageList = document.querySelector('.card-slider .image-container');
+        setDefaultScrollPosition();
+        const scrollableLength = imageList.scrollWidth - imageList.clientWidth;
 
-    // handle sliderbutton onclick
-    const beforeSliderBtn = document.querySelector('#card-slider-before');
-    const nextSliderBtn = document.querySelector('#card-slider-next');
+        // handle sliderbutton onclick
+        const beforeSliderBtn = document.querySelector('#card-slider-before');
+        const nextSliderBtn = document.querySelector('#card-slider-next');
 
-    beforeSliderBtn.addEventListener('click', scrollLeft);
-    nextSliderBtn.addEventListener('click', scrollRight);
+        let index = 0;
 
-})
+        beforeSliderBtn.addEventListener('click', scrollLeft);
+        nextSliderBtn.addEventListener('click', scrollRight);
 
-function scrollLeft(){
-    imageList.scrollBy({
-        left: -200,
-        behaviour: 'smooth',
-    });
-}
+        function setDefaultScrollPosition(){
+            imageList.scrollBy({
+                left: 600,
+                behavior: "smooth"
+            })
+        }
 
-function scrollRight(){
-    imageList.scrollBy({
-        left: 200,
-        behaviour: 'smooth',
-    });
-}
+        console.log(window.scrollX)
+        let scrollable = true;
+        setInterval(function() {
+            if (checkifScrollEnd()){
+                cloneChildtoLast(index);
+                index++;
+                return index;
+            }
+
+        }, 100);
+
+        function scrollLeft(){
+            imageList.scrollBy({
+                left: -400,
+                behaviour: 'smooth',
+            });
+            cloneChildtoStart(index);
+            index++;
+            return index;
+        }
+        
+        function scrollRight(){
+            imageList.scrollBy({
+                left: 400,
+                behaviour: 'smooth',
+            });
+
+            // to create infinite loop
+            cloneChildtoLast(index);
+            index++;
+            return index;
+        }
+
+
+        function cloneChildtoLast(index){
+            imageList.appendChild(imageList.children[index].cloneNode(true));
+            return index;
+        }
+
+        function cloneChildtoStart(index){
+            imageList.prepend(imageList.children[imageList.childElementCount - index - 1].cloneNode(true));
+        }
+    
+
+        function checkifScrollEnd(){
+            if (Math.abs(imageList.scrollLeft) === imageList.scrollWidth - imageList.clientWidth){
+                return true;
+            }
+            else if (Math.abs(imageList.scrollLeft) === imageList.scrollWidth - imageList.clientWidth - 1){
+                return true;
+            }
+            else if (Math.abs(imageList.scrollLeft) === imageList.scrollWidth - imageList.clientWidth + 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+          
+   }) 
+  
+
+
+
